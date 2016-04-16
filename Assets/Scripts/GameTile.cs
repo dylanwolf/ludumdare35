36d370e13d30dace9ff726ef33fba8b77d7f8590
dylan <dylan@dylanwolf.com>
2016-04-16 05:03:51 -0400

    Friday night progress

---
+++ Assets/Scripts/GameTile.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameTile : MonoBehaviour {
+
+	public static List<GameTile> Pool = new List<GameTile>();
+	public static GameTile prefab;
+
+	public Sprite SelectedRowColSprite;
+	public Sprite UnselectedSprite;
+	public Sprite SelectedCellSprite;
+
+	[System.NonSerialized]
+	public int BlockX;
+
+	[System.NonSerialized]
+	public int BlockY;
+
+	SpriteRenderer _r;
+
+	void Awake()
+	{
+		_r = GetComponent<SpriteRenderer>();
+	}
+
+	public void SetSelectedRowCol()
+	{
+		_r.sprite = SelectedRowColSprite;
+	}
+
+	public void SetUnselected()
+	{
+		_r.sprite = UnselectedSprite;
+	}
+
+	public void SetSelectedCell()
+	{
+		_r.sprite = SelectedCellSprite;
+	}
+
+	void OnMouseDown()
+	{
+		GameBoard.Current.SelectBlock(BlockX, BlockY);
+	}
+
+	public static void Despawn(GameTile tile)
+	{
+		GameBoard.Current.Tiles[tile.BlockY, tile.BlockX] = null;
+		tile.gameObject.SetActive(false);
+	}
+
+	void InitializeFromPool(Vector3 localPosition, Transform parent, int x, int y)
+	{
+		BlockX = x;
+		BlockY = y;
+		transform.parent = parent;
+		transform.localPosition = localPosition;
+	}
+
+	static GameTile tmp;
+	public static GameTile Spawn(Vector3 localPosition, Transform parent, int x, int y)
+	{
+		// Attempt to use a pooled block
+		tmp = null;
+		for (int i = 0; i < Pool.Count; i++)
+		{
+			if (Pool[i] != null && !Pool[i].gameObject.activeSelf)
+			{
+				tmp = Pool[i];
+				tmp.gameObject.SetActive(true);
+				tmp.InitializeFromPool(localPosition, parent, x, y);
+				return tmp;
+			}
+		}
+
+		// Create a new block
+		tmp = (GameTile)Instantiate(prefab);
+		tmp.InitializeFromPool(localPosition, parent, x, y);
+		Pool.Add(tmp);
+		return tmp;
+	}
+}
---
+++ Assets/Scripts/SliderArrow.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SliderArrow : MonoBehaviour {
+
+	public static List<SliderArrow> Pool = new List<SliderArrow>();
+	public static SliderArrow prefab;
+
+	[System.NonSerialized]
+	public int DirectionX;
+
+	[System.NonSerialized]
+	public int DirectionY;
+
+	SpriteRenderer _r;
+	Collider2D _c;
+
+	void Awake()
+	{
+		_r = GetComponent<SpriteRenderer>();
+		_c = GetComponent<Collider2D>();
+	}
+
+	void OnMouseDown()
+	{
+		GameBoard.Current.SlideSelection(DirectionX, DirectionY);
+	}
+
+	public static void Despawn(SliderArrow tile)
+	{
+		tile.gameObject.SetActive(false);
+	}
+
+	void InitializeFromPool(Sprite sprite, Vector3 localPosition, Transform parent, int x, int y)
+	{
+		DirectionX = x;
+		DirectionY = y;
+		transform.parent = parent;
+		transform.localPosition = localPosition;
+		_r.sprite = sprite;
+		_r.enabled = false;
+		_c.enabled = false;
+	}
+
+	public void TileSelectionChanged(bool isSelected)
+	{
+		_r.enabled = isSelected;
+		_c.enabled = isSelected;
+	}
+
+	static SliderArrow tmp;
+	public static SliderArrow Spawn(Sprite sprite, Vector3 localPosition, Transform parent, int x, int y)
+	{
+		// Attempt to use a pooled block
+		tmp = null;
+		for (int i = 0; i < Pool.Count; i++)
+		{
+			if (Pool[i] != null && !Pool[i].gameObject.activeSelf)
+			{
+				tmp = Pool[i];
+				tmp.gameObject.SetActive(true);
+				tmp.InitializeFromPool(sprite, localPosition, parent, x, y);
+				return tmp;
+			}
+		}
+
+		// Create a new block
+		tmp = (SliderArrow)Instantiate(prefab);
+		tmp.InitializeFromPool(sprite, localPosition, parent, x, y);
+		Pool.Add(tmp);
+		return tmp;
+	}
+}
